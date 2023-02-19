ea472c2cf55db332497583f34e0580d159ba6125
Abhishek Kumar <115798048+abhiyoke@users.noreply.github.com>
2023-02-19 23:23:25 +0530

    Create count_subseq_having_sumk.cpp

---
+++ count_subseq_having_sumk.cpp
@@ -0,0 +1,47 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) v.begin(),v.end()
+#define ha "YES"
+#define na "NO"
+#define fo(i,a,b) for(int i=a;i<b;i++)
+#define arrin int n;cin>>n;vector<int> a(n);for(int i=0;i<n;i++)cin>>a[i];
+#define sumarr int sum=0;for(int i=0;i<n;i++)sum+=a[i];
+#define forloop for(int i=0;i<n;i++)
+#define allr(v) (v).rbegin(),(v).rend()
+#define vi vector <int>
+#define set_bits __builtin_popcountll
+#define mp map<int,int>
+#define pb push_back
+#define po pop_back
+#define int long long
+#define endl "\n"
+#define F first
+#define S second
+#define mod 1e9 + 7
+#define inf 1e15
+int subksum(int ind,vector<int>&ds,int s,int sum,int a[],int n){
+	if(ind==n){
+		if(s==sum)return 1;
+		else return 0;
+	}
+	ds.pb(a[ind]);
+	s+=a[ind];
+	int l=subksum(ind+1,ds,s,sum,a,n);
+	s-=a[ind];
+		ds.po();
+	int r=subksum(ind+1,ds,s,sum,a,n);
+	return l + r;
+}
+signed main() {
+ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+  
+    int n,sum;
+    cin>>n>>sum;
+    int a[n];
+    fo(i,0,n) cin>>a[i];
+    vector<int>ds;
+    cout<<subksum(0,ds,0,sum,a,n);
+    cout<<endl;
+return 0;    
+}
